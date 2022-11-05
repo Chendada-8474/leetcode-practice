ef8a7af94051e19dff347834f22aa8cf4a7173de
chendada-8474 <raymond96383@gmail.com>
2022-11-06 00:55:04 +0800

    any

--- 79_word_search.py
+++ 79_word_search.py
@@ -2,26 +2,23 @@ from collections import Counter
 
 
 class Solution:
-    def _backtrack(self, b, index):
-        if self.board[b[0]][b[1]] != self.word[index]:
+    def _backtrack(self, x, y, index):
+        if (
+            self.board[x][y] != self.word[index]
+            or self.route
+            or x < 0
+            or y < 0
+            or x >= len(self.board)
+            or y >= len(self.board[0])
+        ):
             return
         index += 1
-        self.route.add(b)
-        if len(self.word) == index:
+        self.route.add((x, y))
+        if len(self.word) == index or any(
+            self._backtrack(x + i, y + j, index) for i, j in self.S
+        ):
             return True
-        for x, y in self.S:
-            x += b[0]
-            y += b[1]
-            if (
-                (x, y) not in self.route
-                and x > -1
-                and y > -1
-                and x < len(self.board)
-                and y < len(self.board[0])
-            ):
-                if self._backtrack((x, y), index):
-                    return True
-        self.route.remove(b)
+        self.route.remove((x, y))
 
     def exist(self, board: list[list[str]], word: str) -> bool:
         self.word = list(word)
@@ -37,7 +34,7 @@ class Solution:
 
         for ixb, xb in enumerate(board):
             for iyb, yb in enumerate(xb):
-                if self._backtrack((ixb, iyb), self.index):
+                if self._backtrack(ixb, iyb, self.index):
                     return True
 
         return False
