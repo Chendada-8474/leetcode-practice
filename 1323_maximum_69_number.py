e9640cbaa93190328326a3b98a4dfed9afb5aa12
chendada-8474 <raymond96383@gmail.com>
2022-11-07 18:08:38 +0800

    69

--- 1323_maximum_69_number.py
+++ 1323_maximum_69_number.py
@@ -1,13 +1,13 @@
 class Solution:
     def maximum69Number(self, num: int) -> int:
-        ans = num
         n = num
         for i in (1000, 100, 10, 1):
             d = int(n / i)
             if d == 6:
-                ans = num + 3 * i if ans < num + 3 * i else ans
+                num = num + 3 * i
+                break
             n = n - d * i
-        return ans
+        return num
 
 
 num = 9996
